e701b65c298d74357787ff2ebb6848387571b2a4
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-11-23 11:57:56 +0900

    initial setting

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,16 @@ module Projects
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :rspec,
+      # テストデータベースにレコードを作成するファイルの作成をスキップ
+      fixtures: false,
+      # ビュースペックを作成しないことを指定
+      view_specs: false,
+      # ヘルパーファイル用のスペックを作成しないことを指定
+      helper_specs: false,
+      # config/routes.rb 用のスペックファイルの作成を省略
+      routing_specs: false
+    end
   end
 end
